4544499af4a2100c595d37ee820dcd7dd7a3d242
VikeshKumar64 <vikeshkumaroriginal@gmail.com>
2026-01-30 23:57:17 +0530

    Day 5 - Codeforces A Square

---
+++ Codeforces/2026-01-30/A _Square.java
@@ -0,0 +1,28 @@
+/* Question Link:- https://codeforces.com/contest/2167/problem/A
+ My Submission:- https://codeforces.com/contest/2167/submission/360766104
+
+
+Thought/Apprroach :-
+No thinking just Brute Force
+*/
+
+import java.util.Scanner;
+
+public class A_Square {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+
+            if(a == b && b == c && c == d){
+                System.out.println("YES");
+            } else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
